b2df93d8874b8257a377dc3822ee2e131219c8af
navdeeptrox-gif <navdeeptrox@gmail.com>
2026-02-16 23:13:45 +0530

    Redirect root to register

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-green-500 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">
-        Sustainovate Running 🚀
-      </h1>
-    </div>
-  );
+  redirect("/register");
 }
